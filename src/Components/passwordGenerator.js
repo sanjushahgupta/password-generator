3aef407c39a8515c2bdc69554fe2e0dc64f83afa
Sanju Shah <sanjushahgupta@gmail.com>
2024-04-15 18:14:56 +0200

    feat: copy password + refactor

--- src/Components/passwordGenerator.js
+++ src/Components/passwordGenerator.js
@@ -1,16 +1,18 @@
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 
 const PasswordGenerator = () => {
-  const [lengthOfPassword, setLengthOfPassword] = useState(8);
-  const [numberAllowed, setNumberAllowed] = useState(false);
-  const [charAllowed, setCharAllowed] = useState(false);
-  const [generatedPassword, setGeneratedPassword] = useState("");
+  const [passwordLength, setpasswordLength] = useState(8);
+  const [includeNumbers, setIncludeNumbers] = useState(false);
+  const [includeSpecialCharacters, setIncludeSpecialCharacters] =
+    useState(false);
+  const [password, setPassword] = useState("");
+  const [copyButtonText, setCopyButtonText] = useState("copy");
 
-  const PasswordGenerator = () => {
+  const generateRandomPassword = () => {
     let newPassword = "";
-    const char = ["$", "@", "!", "*", "?"];
-    const num = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-    let str = [
+    let char = ["$", "@", "!", "*", "?"];
+    let num = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+    let letters = [
       "A",
       "B",
       "C",
@@ -64,66 +66,83 @@ const PasswordGenerator = () => {
       "y",
       "z",
     ];
-    if (charAllowed) str.push(...char);
-    if (numberAllowed) str.push(...num);
-    for (let i = 0; i < lengthOfPassword; i++) {
-      let index = Math.floor(Math.random() * str.length);
-      newPassword += str[index];
-    }
+    if (includeSpecialCharacters) letters.push(...char);
+    if (includeNumbers) letters.push(...num);
 
-    setGeneratedPassword(newPassword);
+    for (let i = 0; i < passwordLength; i++) {
+      let index = Math.floor(Math.random() * letters.length);
+      newPassword += letters[index];
+    }
+    setPassword(newPassword);
   };
 
+  const handleCopyPassword =
+    (async () => {
+      try {
+        await window.navigator.clipboard.writeText(password);
+        setCopyButtonText("copied");
+      } catch (e) {
+        console.error("Failed to copy password:", e);
+      }
+    },
+    [password]);
+
   useEffect(() => {
-    PasswordGenerator();
-  }, [lengthOfPassword, numberAllowed, charAllowed]);
+    generateRandomPassword();
+    setCopyButtonText("copy");
+  }, [passwordLength, includeNumbers, includeSpecialCharacters]);
 
   return (
     <>
-      <div
-        className="w-full max-w-md mx-auto rounded-lg shadow-md px-4  my-8 text-orange-500
+      <div className="min-h-screen flex flex-col items-center justify-center">
+        <div
+          className="w-full max-w-md mx-auto rounded-lg shadow-md px-4  my-8 text-orange-500
   bg-gray-700"
-      >
-        <h1 className="w-full max-w-md  p-4 text-xl text-white">
-          Password Generator
-        </h1>
-        <div className="flex overflow-hidden rounded-lg">
-          <input
-            className="outline-none w-full py-1 px-4 rounded-lg mb-4"
-            value={generatedPassword}
-            type="text"
-            placeholder="password"
-            readOnly
-          ></input>
-          <button className="bg-blue-700 text-white px-3 py-3 mb-4">
-            copy
-          </button>
-        </div>
-        <div className="flex gap-x-2">
-          <input
-            id="lengthOfPassword"
-            className="cursor-pointer"
-            type="range"
-            max={40}
-            min={7}
-            value={lengthOfPassword}
-            onChange={(e) => setLengthOfPassword(e.target.value)}
-          ></input>
-          <label htmlFor="lengthOfPassword">Length:{lengthOfPassword} </label>
-          <input
-            id="numberAllowed"
-            value={numberAllowed}
-            type="checkbox"
-            onClick={(e) => setNumberAllowed((prev) => !prev)}
-          ></input>
-          <label htmlFor="numberAllowed">Numbers</label>
-          <input
-            id="charAllowed"
-            type="checkbox"
-            value={charAllowed}
-            onClick={(e) => setCharAllowed((prev) => !prev)}
-          ></input>
-          <label htmlFor="charAllowed">Characters</label>
+        >
+          <h1 className="w-full max-w-md  p-4 text-xl text-white">
+            Password Generator
+          </h1>
+          <div className="flex overflow-hidden rounded-lg">
+            <input
+              className="outline-none w-full py-1 px-4 rounded-lg mb-4"
+              value={password}
+              type="text"
+              placeholder="password"
+              readOnly
+            ></input>
+            <button
+              onClick={handleCopyPassword}
+              className="bg-blue-700 text-white px-3 py-3 mb-4"
+            >
+              {copyButtonText}
+            </button>
+          </div>
+          <div className="flex gap-x-2 p-4">
+            <input
+              id="lengthOfPassword"
+              className="cursor-pointer"
+              type="range"
+              max={40}
+              min={7}
+              value={passwordLength}
+              onChange={(e) => setpasswordLength(e.target.value)}
+            ></input>
+            <label htmlFor="lengthOfPassword">Length:{passwordLength} </label>
+            <input
+              id="numberAllowed"
+              value={includeNumbers}
+              type="checkbox"
+              onClick={(e) => setIncludeNumbers((prev) => !prev)}
+            ></input>
+            <label htmlFor="numberAllowed">Numbers</label>
+            <input
+              id="charAllowed"
+              type="checkbox"
+              value={includeSpecialCharacters}
+              onClick={(e) => setIncludeSpecialCharacters((prev) => !prev)}
+            ></input>
+            <label htmlFor="charAllowed">Characters</label>
+          </div>
         </div>
       </div>
     </>
